2b869902f766f4c12dfd3c1cca73fb6a4433af7d
kacpermejs <kacpermejs@gmail.com>
2022-05-14 17:09:24 +0200

    insert

--- main.cpp
+++ main.cpp
@@ -8,8 +8,23 @@
 using namespace std;
 
 int Cmax(int* T, int* P, int* X, int M, int N);
+int Cmax1(int* T, int* P, vector<int> X, int M, int N)
+{
+    //zerowanie 
+    for (int i = 0; i <= M; i++) {
+        T[i] = 0;
+    }
+
+    for (int n = 0; n < N; n++) {
+        for (int m = 0; m < M; m++) {
+            T[m + 1] = max(T[m + 1], T[m]) + P[(m)+X[n] * M];
+        }
+    }
+    return T[M];
+}
 int Cmax2(int* T, int* P, int* X, int M, int N)
 {
+    //zerowanie 
     for (int i = 0; i <= M; i++) {
         T[i] = 0;
     }
@@ -129,8 +144,9 @@ void algorytm0(int* T, int* P, int* X, int M, int N,  bool* ordered, vector<pair
     }
 }
 
-void algorytm1(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<int, int>> W, int* temp)//poprawki
+void algorytm1(int* T, int* P, vector<int> &X, int M, int N, bool* ordered, vector<pair<int, int>> W, int* temp)//poprawki
 {
+
     //Wagi
     for (int i = 0; i < N; i++)
     {
@@ -152,13 +168,14 @@ void algorytm1(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<
     //cout << endl;
     int highest = 0;
     int index = -1;
-    int* newX;
-
+    //int* newX;
+    //newX = (int*)malloc(N * sizeof(int));
+    //wersja z std::vector
+    
 
     for (int i = 0; i < N; i++)
     {
 
-        newX = (int*)malloc((i + 1) * sizeof(int));
         
         //index zadania z najwiêksz¹ wag¹ z pozosta³ych
         index = i;// W[i].first;
@@ -167,57 +184,33 @@ void algorytm1(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<
         ordered[index] = true;
         index = W[index].first;
 
-
-
-
         int bestIndex = 0;
         int c;
         int best = INT32_MAX;
 
-        int skip = 0;
-
         for (int k = 0; k < i + 1; k++)//2
         {
-            //Wstawiamy do uszeregowania
-            newX[k] = index;
-            skip = 0;
-            for (int l = 0; l < i; l++)//
-            {
-                if (l == k)
-                    skip = 1;
-                newX[l + skip] = X[l];
-            }
+            //wstawiamy na nowe miejsce
+            X.insert(X.begin() + k, index);
+
             //sprawdzamy czy jest lepiej
-            c = Cmax(T, P, newX, M, i + 1);
+            c = Cmax1(T, P, X, M, i + 1);
             if (c < best)
             {
                 best = c;
                 bestIndex = k;
             }
+            X.erase(X.begin() + k);
+
 
 
         }
-
-
 
         //wynik czêœciowy
-
-        for (int j = 0; j < i + 1; j++)
-        {
-            temp[j] = X[j];
-        }
-        X[bestIndex] = index;
-        skip = 0;
-        for (int j = 0; j < i; j++)//
-        {
-            if (j == bestIndex)
-                skip = 1;
-            X[j + skip] = temp[j];
-        }
-
-        free(newX);
+        X.insert(X.begin() + bestIndex, index);
 
     }
+    
 }
 
 void algorytm2(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<int, int>> W, int* temp)//qneh
@@ -231,7 +224,7 @@ void algorytm2(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<
         ordered[i] = false;
         for (int j = 0; j < M; j++)
         {
-            //W[i] += P[i * M + j];
+            
             W[i].second += P[i * M + j];
 
         }
@@ -240,11 +233,9 @@ void algorytm2(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<
     sort(W.begin(), W.end(), sortbysecdesc);
 
 
-    //cout << endl;
-    int highest = 0;
+    
     int index = -1;
     int* newX;
-
 
     for (int i = 0; i < N; i++)
     {
@@ -269,6 +260,8 @@ void algorytm2(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<
 
         for (int k = 0; k < i + 1; k++)//2
         {
+
+
             //Wstawiamy do uszeregowania
             newX[k] = index;
             skip = 0;
@@ -313,11 +306,16 @@ void algorytm2(int* T, int* P, int* X, int M, int N, bool* ordered, vector<pair<
 
 int main()
 {
-    // N - liczba zadan, M - liczba maszyn, P - czasy na poszczegolnych maszynach (grupowane M)
-    // T - czasy zakonczenia poszczegolnych zadan, X - kolejnosc
-    int N, M, * P, * T, * X;
+    // N - liczba zadan,
+    // M - liczba maszyn, 
+    // P - czasy na poszczegolnych maszynach (grupowane M)
+    // T - czasy zakonczenia dla poszczególnych maszyn (nadpisywana) , 
+    // X - kolejnosc
+    int N, M, * P, * T;
+    int* Xold;
     //int* W;
     vector<pair<int, int>> W;
+    vector<int> X;
 
     bool *ordered;
     int count;
@@ -330,15 +328,17 @@ int main()
     }
     data >> N;
     data >> M;
-    
-    P = new int[M*N];
-    T = new int[M+1];
 
-    X = (int*)malloc(N * sizeof(int));
     int* temp = new int[N];
 
+    P = new int[M*N];
+    T = new int[M+1];
+    
+    Xold = (int*)malloc(N * sizeof(int));
+    X.resize(N);
     //W = new int[N];
     W.resize(N);
+
     ordered = new bool[N];
 
     for (int i = 0; i < M * N; i++) {
@@ -349,7 +349,8 @@ int main()
     
     //algorytm
     auto start = chrono::steady_clock::now();
-    algorytm0(T, P, X, M, N, ordered, W, temp);
+    //algorytm0(T, P, Xold, M, N, ordered, W, temp);
+    algorytm1(T, P, X, M, N, ordered, W, temp);
 
 
     /*
@@ -367,10 +368,12 @@ int main()
     {
         cout << X[i] + 1 << " ";
     }
-    cout << endl << Cmax(T,P,X,M,N) << endl;
+    cout << endl << Cmax1(T,P,X,M,N) << endl;
+    //cout << endl << Cmax(T,P,Xold,M,N) << endl;
     delete[] P;
     delete[] T;
-    free(X);
+    free(Xold);
+    X.clear();
     delete[] temp;
     //delete[] W;
     W.clear();
